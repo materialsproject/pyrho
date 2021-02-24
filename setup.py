5e9b8c45b42fc004c83739258646ec3260206597
jmmshn <jmmshn@lbl.gov>
2021-02-24 15:08:09 -0500

    pulled in from private

--- setup.py
+++ setup.py
@@ -1,41 +1,15 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
 
-from setuptools import setup, find_packages
-from pathlib import Path
+from pkg_resources import VersionConflict, require
+from setuptools import setup
 
-module_dir = Path(__file__).resolve().parent
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
-with open(module_dir / "README.md") as f:
-    long_desc = f.read()
 
-setup(
-    name="pyrho",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    description="Tools for processing volumetric data for material science applications",
-    long_description=long_desc,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jmmshn/pyrho",
-    author="Jimmy Shen",
-    author_email="jmmshn@gmail.com",
-    license="modified BSD",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    zip_safe=False,
-    install_requires=["setuptools"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Machine Learning",
-        "Operating System :: OS Independent",
-        "Topic :: Other/Nonlisted Topic",
-        "Topic :: Database :: Back-Ends",
-        "Topic :: Scientific/Engineering",
-    ],
-    tests_require=["pytest"],
-    python_requires=">=3.7",
-)
+if __name__ == "__main__":
+    setup(use_pyscaffold=True)
--- src/pyrho/core/chargeDensity.py
+++ src/pyrho/core/chargeDensity.py
@@ -2,14 +2,37 @@
 
 """Main module."""
 import math
+from abc import ABCMeta, abstractmethod
+from typing import Dict, List, Union, Iterable
 
-from pymatgen import Lattice, Structure
-from pymatgen.io.vasp import VolumetricData
-from pyrho.core.pgrid import PGrid
 import numpy as np
+from monty.json import MSONable
+from pymatgen import Lattice, Structure
+from pymatgen.io.vasp import VolumetricData, Chgcar, Poscar
+from pyrho.core.pgrid import PGrid
 
 
-class ChargeDensity(PGrid):
+class ChargeABC(metaclass=ABCMeta):
+    @abstractmethod
+    def get_reshaped_cell(
+        self,
+        sc_mat: Iterable = ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+        frac_shift: Iterable = (0, 0, 0),
+        new_grid: Union[List, int] = int(1e9),
+    ):
+        pass
+
+    @abstractmethod
+    def reorient_axis(self) -> None:
+        pass
+
+    @property
+    @abstractmethod
+    def lattice(self) -> Lattice:
+        pass
+
+
+class ChargeDensity(PGrid, ChargeABC):
     def __init__(
         self, grid_data: np.ndarray, structure: Structure, normalization: str = "vasp",
     ):
@@ -72,7 +95,18 @@ class ChargeDensity(PGrid):
             )
 
     @classmethod
-    def from_pmg_volumetric_data(cls, vdata: VolumetricData, data_key="total"):
+    def from_pmg_volumetric_data(
+        cls, vdata: VolumetricData, data_key="total"
+    ) -> "ChargeDensity":
+        """
+        Read a single key from the data field of a VolumetricData object
+        Args:
+            vdata: The volumetric data object
+            data_key: The key to read from in the data field
+
+        Returns:
+            ChargeDensity object
+        """
         return cls(
             grid_data=vdata.data[data_key],
             structure=vdata.structure,
@@ -126,9 +160,15 @@ class ChargeDensity(PGrid):
     #     _, res = get_sc_interp(self.rho, sc_mat, grid_out)
     #     return res.reshape(grid_out)
     #
-    def get_reshaped_cell(self, sc_mat, frac_shift, new_grid=int(1e9)):
+    def get_reshaped_cell(
+        self,
+        sc_mat: Iterable = ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+        frac_shift: Iterable = (0, 0, 0),
+        new_grid: Union[List, int] = int(1e9),
+        up_sample: int = 1,
+    ) -> "ChargeDensity":
         """
-        Motify the structure and data and return a new object containing the reshaped
+        Modify the structure and data and return a new object containing the reshaped
         data
         Args:
             sc_mat: Matrix to create the new cell
@@ -140,22 +180,25 @@ class ChargeDensity(PGrid):
         Returns:
 
         """
-        new_structure = self.structure * sc_mat
+        new_structure = self.structure.copy()
         new_structure.translate_sites(
             list(range(len(new_structure))), -np.array(frac_shift)
         )
+        new_structure = new_structure * sc_mat
 
         # determine the output grid
         lengths = new_structure.lattice.abc
         if isinstance(new_grid, int):
             ngrid = new_grid / new_structure.volume
             mult = (np.prod(lengths) / ngrid) ** (1 / 3)
-            grid_out = [int(math.floor(max(l / mult, 1))) for l in lengths]
+            grid_out = [int(math.floor(max(l_ / mult, 1))) for l_ in lengths]
         else:
             grid_out = new_grid
 
-        new_rho = self.get_transformed_data(sc_mat, frac_shift, grid_out=grid_out)
-        return self.from_rho(new_rho, new_structure, self.normalization)
+        new_rho = self.get_transformed_data(
+            sc_mat, frac_shift, grid_out=grid_out, up_sample=up_sample
+        )
+        return ChargeDensity.from_rho(new_rho, new_structure, self.normalization)
 
     #
     #     _, new_rho = get_sc_interp(self.rho, sc_mat, grid_sizes=grid_out)
@@ -167,3 +210,93 @@ class ChargeDensity(PGrid):
     #
     #     new_rho = roll_array(new_rho, grid_shifts)
     #     return self.__class__.from_rho(new_rho, new_structure)
+
+
+class SpinChargeDensity(MSONable, ChargeABC):
+    def __init__(self, chargeden_dict: Dict, aug_charge: Dict = None):
+        """
+        Wrapper class that parses multiple sets of grid data on the same lattice
+
+        Args:
+            chargeden_dict: A dictionary containing multiple charge density objects
+                        typically in the format {'total' : ChargeDen1, 'diff' : ChargeDen2}
+        """
+        self.chargeden_dict = chargeden_dict
+        self.aug_charge = aug_charge
+        self._tmp_key = next(
+            iter(self.chargeden_dict)
+        )  # get one key in the dictionary to make writing the subsequent code easier
+
+    @classmethod
+    def from_pmg_volumetric_data(
+        cls, vdata: VolumetricData, data_keys=("total", "diff")
+    ):
+        chargeden_dict = {}
+        data_aug = getattr(vdata, "data_aug", None)
+        for k in data_keys:
+            chargeden_dict[k] = ChargeDensity.from_pmg_volumetric_data(vdata, data_key=k)
+        return cls(chargeden_dict, aug_charge=data_aug)
+
+    @property
+    def lattice(self) -> Lattice:
+        return self.chargeden_dict[self._tmp_key].lattice
+
+    def to_Chgcar(self) -> Chgcar:
+        struct = self.chargeden_dict[self._tmp_key].structure
+        data_ = {k: v.renormalized_data for k, v in self.chargeden_dict.items()}
+        return Chgcar(Poscar(struct), data_, data_aug=self.aug_charge)
+
+    def to_VolumetricData(self) -> VolumetricData:
+        key_ = next(iter(self.chargeden_dict))
+        struct = self.chargeden_dict[key_].structure
+        data_ = {k: v.renormalized_data for k, v in self.chargeden_dict.items()}
+        return VolumetricData(struct, data_)
+
+    def get_reshaped_cell(
+        self,
+        sc_mat: Iterable = ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+        frac_shift: Iterable = (0, 0, 0),
+        new_grid: Union[List, int] = int(1e9),
+    ) -> "SpinChargeDensity":
+        new_spin_charge = {}
+        for k, v in self.chargeden_dict.items():
+            new_spin_charge[k] = v.get_reshaped_cell(sc_mat, frac_shift, new_grid)
+        factor = int(
+            new_spin_charge[self._tmp_key].structure.num_sites
+            / self.chargeden_dict[self._tmp_key].structure.num_sites
+        )
+        new_aug = {}
+        if self.aug_charge is not None:
+            for k, v in self.aug_charge.items():
+                new_aug[k] = multiply_aug(v, factor)
+        return SpinChargeDensity(new_spin_charge, new_aug)
+
+    def reorient_axis(self) -> None:
+        for k, v in self.chargeden_dict:
+            v.reorient_axis()
+
+
+def multiply_aug(data_aug, factor):
+    res = []
+    cur_block = None
+    cnt = 0
+    for ll in data_aug:
+        if "augmentation" in ll:
+            if cur_block:
+                for j in range(factor):
+                    cnt += 1
+                    cur_block[
+                        0
+                    ] = f"augmentation occupancies{cnt:>4}{cur_block[0].split()[-1]:>4}\n"
+                    res.extend(cur_block)
+            cur_block = [ll]
+        else:
+            cur_block.append(ll)
+    else:
+        for j in range(factor):
+            cnt += 1
+            cur_block[
+                0
+            ] = f"augmentation occupancies{cnt:>4}{cur_block[0].split()[-1]:>4}\n"
+            res.extend(cur_block)
+    return res

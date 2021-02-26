a0559c5d0f43307e344a3acab03fd66676019ffa
jmmshn <jmmshn@gmail.com>
2021-02-26 16:10:09 -0500

    cleanup

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-""" Primary PyRho module """
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass

7239c4c5ef52a9275464109818e58fc259ba1fe3
mat-elisee <matthias.elisee@outlook.com>
2022-04-15 11:05:50 +0200

    Better tests

--- tests/core/test_charge_density.py
+++ tests/core/test_charge_density.py
@@ -1,20 +1,20 @@
 import numpy as np
-from hypothesis import given, seed
-from hypothesis import strategies as st
 from pymatgen.io.vasp import Chgcar
 
 from pyrho.core.chargeDensity import ChargeDensity
 
+global TESTFILE
+TESTFILE = "../../test_files/Si.uc.hdf5"
 
 def test_density_copy():
-    chgcar: Chgcar = Chgcar.from_hdf5("../../test_files/Si.uc.hdf5")
+    chgcar: Chgcar = Chgcar.from_hdf5(TESTFILE)
     density = ChargeDensity.from_pmg_volumetric_data(chgcar)
 
     density_copy = density.from_rho(density.rho, density.structure, normalization=density.normalization)
     assert np.array_equal(density.rho, density_copy.rho)
 
 def test_density_transformed():
-    chgcar: Chgcar = Chgcar.from_hdf5("../../test_files/Si.uc.hdf5")
+    chgcar: Chgcar = Chgcar.from_hdf5(TESTFILE)
     density = ChargeDensity.from_pmg_volumetric_data(chgcar)
 
     sc_mat = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
@@ -22,7 +22,7 @@ def test_density_transformed():
     assert density_transformed.structure == density.structure * sc_mat
 
 def test_density_chgcar():
-    chgcar: Chgcar = Chgcar.from_hdf5("../../test_files/Si.uc.hdf5")
+    chgcar: Chgcar = Chgcar.from_hdf5(TESTFILE)
     density = ChargeDensity.from_pmg_volumetric_data(chgcar)
 
     assert density.to_Chgcar() == chgcar

3ec1270c95d87925975f7424f759d54142b8286a
mat-elisee <matthias.elisee@outlook.com>
2022-04-15 11:22:12 +0200

    Wrong test removal

--- tests/core/test_charge_density.py
+++ tests/core/test_charge_density.py
@@ -6,12 +6,14 @@ from pyrho.core.chargeDensity import ChargeDensity
 global TESTFILE
 TESTFILE = "../../test_files/Si.uc.hdf5"
 
+
 def test_density_copy():
     chgcar: Chgcar = Chgcar.from_hdf5(TESTFILE)
     density = ChargeDensity.from_pmg_volumetric_data(chgcar)
 
     density_copy = density.from_rho(density.rho, density.structure, normalization=density.normalization)
     assert np.array_equal(density.rho, density_copy.rho)
+
 
 def test_density_transformed():
     chgcar: Chgcar = Chgcar.from_hdf5(TESTFILE)
@@ -20,9 +22,3 @@ def test_density_transformed():
     sc_mat = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
     density_transformed = density.get_transformed_obj(sc_mat=sc_mat)
     assert density_transformed.structure == density.structure * sc_mat
-
-def test_density_chgcar():
-    chgcar: Chgcar = Chgcar.from_hdf5(TESTFILE)
-    density = ChargeDensity.from_pmg_volumetric_data(chgcar)
-
-    assert density.to_Chgcar() == chgcar

777e43ec17b802ab6afe3b04008abe084711d320
Seán Kavanagh <51478689+kavanase@users.noreply.github.com>
2024-09-24 16:03:52 -0400

    Linting update

--- src/pyrho/__init__.py
+++ src/pyrho/__init__.py
@@ -3,7 +3,7 @@
 __author__ = """Jimmy Shen"""
 __email__ = "jmmshn@gmail.com"
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version(__name__)
